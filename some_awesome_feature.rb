add31d8a243d6b16eb81ea44a4818a07976f489e
Robert Williford <rswillif@Roberts-MacBook-Pro.local>
2016-09-16 12:15:32 -0400

    nope

--- some_awesome_feature.rb
+++ some_awesome_feature.rb
@@ -1,14 +1,3 @@
-puts "Who lives in a pineapple under the sea?"
-
-def main()
-  #todo write my program
-end
-
-if __FILE__ == $PROGRAM_NAME
-  main
-end
-
-# hi guys! -fitchy
 # nope
 
 _______________________$$$$$$_________$$$$$$
